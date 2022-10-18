a543867ec67ddeb67253b2cda063a4789dae69ec
Donnie Moy <donniem@nycstudents.net>
2022-10-18 12:27:44 -0400

    Initial commit

---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome!");
+
+        System.out.println("Enter coordinate 1: ");
+        String co1 = scanner.nextLine();
+
+    }
+}
